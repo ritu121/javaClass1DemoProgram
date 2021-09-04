969c5fea8d4444e188ec0cb96dae89a973fda64a
Food Website <riturandive121@gmail.com>
2021-09-04 18:06:17 +0530

    Initial Commit

---
+++ AddingTwoNumber.java
@@ -0,0 +1,11 @@
+package demo2;
+
+public class AddingTwoNumber {
+
+	 public static void main(String args[]) {
+		 int a=12;
+		 int b=16;
+		 
+		 System.out.print(a+b); 
+	 }
+}
---
+++ FindSamllestNumber.java
@@ -0,0 +1,17 @@
+package demo2;
+
+public class FindSamllestNumber {
+	 public static void main(String args[]) {
+		 int num1 =127;
+		 int num2 =14;
+		 
+		 if(num1<num2) {
+			 System.out.print(num1); 
+		 }
+		 else {
+			 System.out.print(num2); 
+		 }
+		 
+	 }
+
+}
